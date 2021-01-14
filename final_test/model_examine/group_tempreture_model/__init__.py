f7163fca026bcc574635c8d4cb2ab256371537a0
markduan <dcx5601@163.com>
2021-01-13 21:29:34 -0800

    final test examine

---
+++ final_test/model_examine/group_tempreture_model/__init__.py
@@ -0,0 +1,124 @@
+import math
+
+class OptimalGroupTempreture():
+    def __init__(self, minTs, maxTs, sensitivity=1.0, defaultMinMaxRange=1.0):
+        """
+        sensitivity > 0, less means more sensitive
+        defaultMinMaxRange > 0 or == None
+        """
+        if len(minTs) != len(maxTs):
+            print('Length of min tempreture records and max tempretur records are not equal!')
+            return None
+        self.minTs = minTs
+        self.maxTs = maxTs
+        self.sensitivity = sensitivity
+        self.defaultMinMaxRange = defaultMinMaxRange
+    
+    def getTempreture(self, model='auto', sensitivePeoplePercentageLimit=0.1, sensitiveRange=2, ppd=0.5):
+        """
+        model = 'auto', 'sensitive first', 'otd model'
+        sensitivePeoplePercentage is > 0 and < 1
+        sensitiveRange is > 0, unit is Fahrenheit Degree
+        ppd: only for OTD model
+        """
+        minTs = self.minTs
+        maxTs = self.maxTs
+
+        if self.defaultMinMaxRange is not None:
+            avgTs = [(maxTs[i] + minTs[i]) / 2 for i in range(len(minTs))]
+            tRanges = [maxTs[i] - minTs[i] for i in range(len(minTs))]
+            checkedtRanges = []
+            for i in range(len(tRanges)):
+                t = tRanges[i] if tRanges[i] > 0 else self.defaultMinMaxRange
+                checkedtRanges.append(t)
+        else:
+            avgTs = []
+            tRanges = [maxTs[i] - minTs[i] for i in range(len(minTs))]
+            checkedtRanges = []
+            for i in range(len(tRanges)):
+                if tRanges[i] > 0:
+                    t = tRanges[i]
+                    checkedtRanges.append(t)
+                    avgTs.append((maxTs[i] + minTs[i]) / 2)
+
+        # model selection
+        if model == 'sensitive first':
+            print('sensitive first model')
+            targetTempreture = self.sensitiveFirst(checkedtRanges, avgTs)
+        elif model == 'otd model':
+            print('otd model')
+            targetTempreture = self.otdModel(checkedtRanges, avgTs, ppd)
+        elif model == 'auto':
+            lengthOfSensitivePeople = len([tRange for tRange in checkedtRanges if tRange <= sensitiveRange])
+            print('sensitive people percentage:', lengthOfSensitivePeople / len(checkedtRanges))
+            if lengthOfSensitivePeople / len(checkedtRanges) >= sensitivePeoplePercentageLimit:
+                print('sensitive first model')
+                print('sensitive people percentage:', lengthOfSensitivePeople / len(checkedtRanges))
+                targetTempreture = self.sensitiveFirst(checkedtRanges, avgTs)
+            else:
+                print('otd model')
+                targetTempreture = self.otdModel(checkedtRanges, avgTs, ppd)
+                if targetTempreture is None:
+                    print('otd model does not find a acceptable value, use sensitive first model')
+                    targetTempreture = self.sensitiveFirst(checkedtRanges, avgTs)
+        else:
+            print('Wrong model')
+            return None
+
+        return targetTempreture
+
+    def sensitiveFirst(self, checkedtRanges, averageTs):
+        weights = [self.computeWeight(tRange) for tRange in checkedtRanges]
+        weightedTs = [averageTs[i] * weights[i] for i in range(len(averageTs))]
+        targetTempreture = sum(weightedTs) / sum(weights)
+        return targetTempreture
+
+    def computeWeight(self, tRange):
+        return (1 / tRange) ** self.sensitivity
+
+    def otdModel(self, checkedtRanges, averageTs, ppd):
+        numberOfPeople = len(checkedtRanges)
+        permitNumberOfUncomfortablePeople = math.floor(numberOfPeople * ppd)
+        otdMax = (numberOfPeople - permitNumberOfUncomfortablePeople) * 1 + \
+            permitNumberOfUncomfortablePeople * (numberOfPeople + 1)
+
+        rangeTs = [self.otdDecideComfortness(checkedtRanges[i], averageTs[i]) for i in range(len(checkedtRanges))]
+
+        # find minT and maxT
+        minT = min([averageTs[i] - checkedtRanges[i] / 2 for i in range(len(checkedtRanges))])
+        maxT = max([averageTs[i] + checkedtRanges[i] / 2 for i in range(len(checkedtRanges))])
+
+        acceptableTs = []
+        for t10 in range(int(minT * 10), int(maxT * 10) + 1):
+            t = t10 / 10
+            otd = 0
+            for x in rangeTs:
+                if t >= x[0][0] and t <= x[0][1]:
+                    otd += 0
+                elif t >= x[1][0] and t <= x[1][1]:
+                    otd += 1
+                elif t >= x[2][0] and t <= x[2][1]:
+                    otd += numberOfPeople + 1
+                else:
+                    otd = (numberOfPeople + 1) * 2
+            if otd <= otdMax:
+                acceptableTs.append([t, otd])
+        if acceptableTs == []:
+            return None
+        acceptableTs.sort(key=lambda x: x[1])
+        maxValues = [acceptableTs[0][0]]
+        for i in range(1, len(acceptableTs)):
+            if acceptableTs[i][1] == acceptableTs[0][1]:
+                maxValues.append(acceptableTs[i][0])
+#         print(acceptableTs)
+        return sum(maxValues) / len(maxValues)
+
+                
+    def otdDecideComfortness(self, checkedtRange, averageT, \
+                            littleUncomfortable=[1 , 1.4], \
+                            uncomfortable=[1.4, 1.7]):
+        radius = checkedtRange / 2
+        rangeComfortable = [averageT - radius * littleUncomfortable[0], averageT + radius * littleUncomfortable[0]]
+        rangeLittleUncomfortable = [averageT - radius * littleUncomfortable[1], averageT + radius * littleUncomfortable[1]]
+        rangeUncomfortable = [averageT - radius * uncomfortable[1], averageT + radius * uncomfortable[1]]
+        return [rangeComfortable, rangeLittleUncomfortable, rangeUncomfortable]
